3a9dc6b3389baf885b8c4589de7430dd68d6b669
Mani Movassagh <mani.mgh@gmail.com>
2024-09-14 00:55:36 +0200

    service

--- todo-resource/controllers/TodoController.cs
+++ todo-resource/controllers/TodoController.cs
@@ -1,19 +1,14 @@
 using Microsoft.AspNetCore.Mvc;
 
 
-    [ApiController]
-    [Route("/")]
-    public class TodoController : ControllerBase
+[ApiController]
+[Route("/")]
+public class TodoController : ControllerBase
+{
+    [HttpGet]
+    public IEnumerable<ToDO> Get()
     {
-        [HttpGet]
-        public IEnumerable<ToDO> Get()
-        {
-            return new List<ToDO>
-            {
-                new (1, "Learn .NET", false),
-                new (2, "Build API and see how good ", true),
-                new (3, "Test Application", false)
-            };
-        }
+        return TodoService.GetToDOs();
     }
+}
 
---
+++ todo-resource/seervices/ToDoService.cs
@@ -0,0 +1,20 @@
+using Microsoft.AspNetCore.Mvc;
+
+
+
+
+    public static class TodoService
+    {
+
+        public static List<ToDO> GetToDOs()
+        {
+            return new List<ToDO>
+            {
+                new (1, "Learn .NET", false),
+                new (2, "Build API and see how good ", true),
+                new (3, "Test Application", false),
+                new (4, "Service is working", false),
+            };
+        }
+    }
+
