a77cae6c35b8a19d9b2ac3465909a46e98962532
Mani Movassagh <mani.mgh@gmail.com>
2024-09-14 19:59:14 +0200

    simplified

--- todo-resource/seervices/ToDoService.cs
+++ todo-resource/seervices/ToDoService.cs
@@ -2,17 +2,18 @@
 
 
 
-    public static class TodoService
+public static class TodoService
+{
+    public static List<ToDO> GetToDOs()
     {
-        public static List<ToDO> GetToDOs()
-        {
-            return new List<ToDO>
-            {
-                new (1, "Learn .NET is fun", false),
+        return
+        [
+            new (1, "Learn .NET is fun", false),
                 new (2, "Build API and see how good ", true),
                 new (3, "Test Application", false),
                 new (4, "Service is working", false),
-            };
-        }
+                new (5,"Mani is awesome",true),
+            ];
     }
+}
 
